22a1a0871e4af13be0cbfe4e2077ef0dbb8f9f88
Mia Bosheva <boshevam@gmail.com>
2024-08-04 18:01:16 +0200

    Initial setup

---
+++ MyRoutes/MyRoutesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRoutesApp.swift
+//  MyRoutes
+//
+//  Created by Mia on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MyRoutesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
